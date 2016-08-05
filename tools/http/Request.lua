b62fcfb924d81415f861fe6f302e6a1b992dc184
Anatolii Yakushko <ayakushko@intropro.com>
2016-08-05 18:06:39 +0300

    [-] request fixed

--- tools/http/Request.lua
+++ tools/http/Request.lua
@@ -1,8 +1,11 @@
 local StringTools = require("tools/StringTools")
+local DIVIDER="\n"
+local HEADER_DIVIDER=DIVIDER..DIVIDER
 local Request = {
-    parse=function(self, request)
-        local chunks = StringTools.split(request, "\n")
 
+    parse=function(self, request)
+        //string.gsub(request
+        local chunks = StringTools.split(request, HEADER_DIVIDER)
         return {
             chunks=chunks
         }
